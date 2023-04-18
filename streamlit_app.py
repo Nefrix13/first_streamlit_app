8dfd2c747527059edcb303697e90ab8a0e6511be
Nefrix13 <52854939+Nefrix13@users.noreply.github.com>
2023-04-18 11:39:57 +0200

    Update streamlit_app.py
    read data from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
+import pandas
 
 streamlit.title("My parents New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-#streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
