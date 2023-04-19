db74d26287b933f107bced6fc96bd570b96e7218
Nefrix13 <52854939+Nefrix13@users.noreply.github.com>
2023-04-19 15:29:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,9 @@ streamlit.dataframe(fruits_to_show)
 
 import requests 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # write your own comment -what does the next line do? 
